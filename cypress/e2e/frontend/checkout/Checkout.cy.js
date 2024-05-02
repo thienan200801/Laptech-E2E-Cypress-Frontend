a3e589fa0d319cd594bd21c3d07c934cd069d12b
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2024-05-02 20:50:50 +0700

    feature: add auto for Register feature

---
+++ cypress/e2e/frontend/checkout/Checkout.cy.js
@@ -0,0 +1,9 @@
+describe('', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/cart');
+        cy.wait(2000);
+    })
+    it('As a user, I want to checkout with existing items in my cart', () => {
+        
+    })
+})
